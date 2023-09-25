f866eee2fe37f84aaf161dba272e617f8cd8c9be
VoldyMoldy <thdidhc@gmail.com>
2023-09-25 12:55:02 -0400

    finished event implementation

--- game.py
+++ game.py
@@ -37,10 +37,11 @@ stocks = []
 
 class stock():
     def __init__(self, name: str, init_val: int, chng_amt: int):
-        self.type       = name       #name of stock
-        self.val        = init_val   #initial value 
-        self.inc_chance = 50         #chance of incrementing positively (edited by events)
-        self.chng_amt   = chng_amt   #maximum amount to change by either up or down (also edited by events)
+        self.type           = name       #name of stock
+        self.val            = init_val   #initial value 
+        self.inc_chance     = 50         #chance of incrementing positively (edited by events)
+        self.chng_amt       = chng_amt   #maximum amount to change by either up or down (also edited by events)
+        self.reset_chng_amt = chng_amt   #save increment value in case of events
         stocks.append(self)
 
     def update(self):
@@ -48,29 +49,41 @@ class stock():
             self.val += r.randint(0, self.chng_amt)
         else:
             self.val -= r.randint(0, self.chng_amt)
+    
+    def reset(self):
+        self.inc_chance = 50
+        self.chng_amt   = self.reset_chng_amt
 
-forest_stock = stock('GameStop', 10, 1)
+gamestop_stock = stock('GameStop', 10, 1)
 
 #events
 events = []
+current_event = None
 
 class event():
-    def __init__(self, targets: list, name: str, desc: str, duration: int):
-        self.targets = targets   #types of stock to target
-        self.name    = name      #name of event to be displayed
-        self.desc    = desc      #description to be displayed
-        self.dur     = duration  #in seconds, how long the event lasts
+    def __init__(self, targets: list, name: str, desc: str, duration: int, new_inc: int, new_chng: int):
+        self.targets  = targets  #types of stock to target
+        self.name     = name     #name of event to be displayed
+        self.desc     = desc     #description to be displayed
+        self.dur      = duration #in seconds, how long the event lasts
+        self.new_inc  = new_inc  #new chance to increment up
+        self.new_chng = new_chng #new increment amount
 
-    def trigger(self, old_inc: int, new_inc: int, old_chng: int, new_chng: int, start_time: int):
-        self.old_inc  = 0          #save original stock incrememnt chance to reset after event
-        self.new_inc  = new_inc    #new chance to increment up
-        self.old_chng = 0          #save old increment amount to reset after event
-        self.new_chng = new_chng   #new increment amount
+    def trigger(self, start_time: int):
         self.start    = start_time #save start time of event to keep track of duration
-        for stock in stocks:       #iterate through all stocks to find targets, save off their original stats and replace them for the event
+        for stock in stocks:       #iterate through all stocks to find targets, replace stats for the event
             if stock.type in self.targets:
-                self.old_inc  = stock.inc_chance; stock.inc_chance = self.new_inc
-                self.old_chng = stock.chng_amt;   stock.chng_amt   = self.new_chng
+                stock.inc_chance = self.new_inc
+                stock.chng_amt   = self.new_chng
+
+    def check_end(self): #check if event is ended, if so reset changed stock stats and set current event to nothing
+        if TIME - self.dur == self.start:
+            for stock in stocks:
+                if stock.type in self.targets:
+                    stock.reset()
+            current_event = None
+
+wallstreet_bets = event(['Gamestop'], 'Wallstreet Bets Strikes Again!', 'Of course it\'s Gamestop...', 30, 4, 80)
 
 #ui
 elements = []
@@ -86,8 +99,11 @@ class panel(pygame.sprite.Sprite):
         self.surf.blit(self.label, label_pos)                        #applies label to panel
         elements.append(self)
 
-inc_panel   = panel(0          , 0           , 400, 900, (150, 150, 150), 'Businesses'    , (50, 50))
-event_panel = panel(WIDTH - 400, HEIGHT - 200, 400, 200, (100, 100, 100), 'Current Event:', (25, 25)) 
+inc_panel   = panel(0          , 0           , 400, 900, (150, 150, 150), 'Businesses'            , (50, 50))
+event_panel = panel(WIDTH - 400, HEIGHT - 200, 400, 200, (100, 100, 100), 'Current Event:\nNothing', (25, 25))
+
+if current_event != None:
+    event_panel.label = 'Current Event:\n' + current_event.name + '\n' + current_event.desc
 
 #draw ui elements
 def draw_ui():
@@ -106,8 +122,15 @@ while True:
     TICK += 1
     #update stocks each second
     if TICK % FPS == 0:
+        TIME += 1
         for stock_type in stocks:
             stock_type.update()
+        if current_event != None:
+            current_event.check_end()
+    #run event every minute except for the first
+    if TIME != 0 and TIME % 60 == 0:
+        current_event = events[r.randint(0, len(events) - 1)]
+        current_event.trigger(TIME)
     #update screen
     #bg
     displaysurface.fill((230, 230, 230))
