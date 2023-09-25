10718f508c4d319adfc7c5c604c858008608b2ed
VoldyMoldy <thdidhc@gmail.com>
2023-09-25 19:51:57 -0400

    fleshed out events

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ WIDTH  = 1600
 FPS    = 60
 
 #text setup
-FONT   = pygame.font.SysFont('Congenial', 50)
+FONT   = pygame.font.SysFont('Congenial', 35)
 
 #time related vars
 TICK   = 0 #1/FPS of a second, used to determine a second
@@ -36,7 +36,7 @@ pygame.display.set_caption("Game")
 stocks = []
 
 class stock():
-    def __init__(self, name: str, init_val: int, chng_amt: int):
+    def __init__(self, name: str, init_val: int, chng_amt: float):
         self.type           = name       #name of stock
         self.val            = init_val   #initial value 
         self.inc_chance     = 50         #chance of incrementing positively (edited by events)
@@ -46,15 +46,24 @@ class stock():
 
     def update(self):
         if r.randint(1, 100) <= self.inc_chance:
-            self.val += r.randint(0, self.chng_amt)
+            self.val += r.random() * self.chng_amt
         else:
-            self.val -= r.randint(0, self.chng_amt)
+            self.val -= r.random() * self.chng_amt
+        if self.val < 0:
+            self.val = 0
     
     def reset(self):
         self.inc_chance = 50
         self.chng_amt   = self.reset_chng_amt
 
-gamestop_stock = stock('GameStop', 10, 1)
+gamestop_stock = stock('GameStop', 10, 0.2)
+startup_stock  = stock('Random Startup Company', 35, 0.7) #need to change name
+twitter_stock  = stock('X (formerly Twitter)', 75, 3)     #can change any of these next 3 to whatever if wanted, just have them be in ascending value
+twitter_stock.inc_chance = 45 #stock is not doing so hot
+apple_stock    = stock('Apple', 150, 7.5)
+apple_stock.inc_chance = 55 #is doing good
+google_stock   = stock('Google', 500, 60)
+google_stock.inc_chance  = 60 #top value stock is doing very well
 
 #events
 events = []
@@ -77,13 +86,15 @@ class event():
                 stock.chng_amt   = self.new_chng
 
     def check_end(self): #check if event is ended, if so reset changed stock stats and set current event to nothing
+        global current_event
         if TIME - self.dur == self.start:
             for stock in stocks:
                 if stock.type in self.targets:
                     stock.reset()
             current_event = None
 
-wallstreet_bets = event(['Gamestop'], 'Wallstreet Bets Strikes Again!', 'Of course it\'s Gamestop...', 30, 4, 80)
+wallstreet_bets = event(['Gamestop'], 'Wallstreet Bets Strikes Again!', 'Of course its Gamestop...'          , 30, .5, 80)
+controversy     = event(['Gamestop'], 'Controversial Decision!'       , 'Way to lose your customers to greed', 40, .3, 20)
 
 #ui
 elements = []
@@ -93,17 +104,46 @@ class panel(pygame.sprite.Sprite):
         super().__init__()
         self.pos   = (x, y)                                          #position of top left corner of panel on screen, starts in top left
         self.surf  = pygame.Surface((w, h))                          #creates surface to be drawn to of width w and height h
+        self.color = color                                           #save color for updating
         self.surf.fill(color)                                        #fills in surface with color
         self.rect  = self.surf.get_rect(center = (x + w/2, y + h/2)) #rect to draw to on game loop
-        self.label = FONT.render(label, True, (0, 0, 0))             #creates label for the panel
-        self.surf.blit(self.label, label_pos)                        #applies label to panel
+        self.label = FONT.render(label, True, (0, 0, 0))
+        self.label_pos = label_pos                                   #creates label for the panel and saves pos for updating
+        self.surf.blit(self.label, self.label_pos)                   #applies label to panel
         elements.append(self)
 
-inc_panel   = panel(0          , 0           , 400, 900, (150, 150, 150), 'Businesses'            , (50, 50))
-event_panel = panel(WIDTH - 400, HEIGHT - 200, 400, 200, (100, 100, 100), 'Current Event:\nNothing', (25, 25))
+    def update_label(self, new_label: str):
+        self.surf.fill(self.color)                           #clear old text by filling over it
+        self.label = FONT.render(new_label, True, (0, 0, 0)) #recreate label with new text
+        self.surf.blit(self.label, self.label_pos)           #draw text to surface
 
-if current_event != None:
-    event_panel.label = 'Current Event:\n' + current_event.name + '\n' + current_event.desc
+#business panels
+inc_panel        = panel(0          , 0           , 400, 900, (150, 150, 150), 'Businesses'    , (50, 50))
+
+#event panels
+event_panel      = panel(WIDTH - 400, HEIGHT - 200, 400, 200, (100, 100, 100), 'Current Event:', (25, 25))
+event_name_panel = panel(WIDTH - 400, HEIGHT - 133, 400, 133, (100, 100, 100), '',               (25, 25)) #label not working
+event_desc_panel = panel(WIDTH - 400, HEIGHT -  67, 400, 67 , (100, 100, 100), '',               (25, 25)) #label not working
+
+#stock panels
+gamestop_name_panel = panel(400, 0          , 800, HEIGHT / 10, (230, 230, 230), 'GameStop: $' + str(gamestop_stock.val), (25, 25))
+gamestop_info_panel = panel(400, HEIGHT / 10, 800, HEIGHT / 5 , (230, 230, 230), 'You have ' + str(STOCK_QUANTS[0]) + ' stocks for a total of $' + str(STOCK_QUANTS[0] * gamestop_stock.val), (25, 25))
+
+#event panel label debug
+current_event = wallstreet_bets
+current_event.start = 0
+
+#hardcoded for now, maybe automate later
+def update_panel_labels():
+    gamestop_name_panel.update_label('GameStop: $' + ("%.2f" % gamestop_stock.val))
+    gamestop_info_panel.update_label('You have ' + str(STOCK_QUANTS[0]) + ' stocks for a total of $' + ("%.2f" % (STOCK_QUANTS[0] * gamestop_stock.val)))
+    
+    if current_event != None:
+        event_name_panel.update_label(current_event.name)
+        event_desc_panel.update_label(current_event.desc)
+    else:
+        event_name_panel.update_label('')
+        event_desc_panel.update_label('')
 
 #draw ui elements
 def draw_ui():
@@ -120,8 +160,8 @@ while True:
 
     #uptade tick
     TICK += 1
-    #update stocks each second
-    if TICK % FPS == 0:
+    #update stocks every other second
+    if TICK % (2 * FPS) == 0:
         TIME += 1
         for stock_type in stocks:
             stock_type.update()
@@ -135,7 +175,11 @@ while True:
     #bg
     displaysurface.fill((230, 230, 230))
     #elements
+    update_panel_labels()
     draw_ui()
+
+    #stock debugging
+    print(str(gamestop_stock.val) + ' ' + str(STOCK_QUANTS[0]) + ' ' + str(gamestop_stock.inc_chance))
 
     pygame.display.update()
     FramePerSec.tick(FPS)
